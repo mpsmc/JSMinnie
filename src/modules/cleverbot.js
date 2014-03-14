7ea72282de1b5c389e960d5e6d4da02fbaa40a9c
r04r <r04r@minichan.org>
2014-03-14 19:36:42 +0100

    cleverbot

---
+++ src/modules/cleverbot.js
@@ -0,0 +1,16 @@
+var CleverBot = new require('cleverbot-node');
+var clever = new CleverBot();
+
+module.exports = function(client, config, jb) {
+	client.addListener('message', function (from, to, message) {
+		if(to.indexOf("#") !== 0) return;
+		var match = /^minnie:? (.+)/i.exec(message);
+
+		if(match !== null) {
+			clever.write(match[1], function(data) {
+				console.log(data);
+				client.say(to, from + ": " + data.message);
+			});
+		}
+	});
+};
