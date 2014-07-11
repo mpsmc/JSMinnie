077db361cdeb47439b28735ea5dc1713a74b37c4
r04r <r04r@minichan.org>
2014-07-11 21:25:12 +0200

    updated linkwatch

--- src/modules/linkwatch.js
+++ src/modules/linkwatch.js
@@ -6,6 +6,7 @@ var request = require('request').defaults({
 
 var cheerio = require('cheerio');
 var rurl = require('url');
+var async = require('async');
 
 module.exports = function(client, config) {
 	var linkCache = {};
@@ -24,26 +25,23 @@ module.exports = function(client, config) {
 	client.addListener('message', function (from, to, message) {
 		var urls = message.match(URL_REGEX);
 		if(urls != null) {
-			var messages = [];
-			var counter = 0;
-			var cb = function(err, message) {
-				if(!err)
-					messages.push(message);
-				else
-					console.error(err);
-
-				counter++;
-				if(counter == urls.length && messages.length > 0) {
+			async.map(urls, urlHandler, function(err, results) {
+				if(err) {
+					client.say(to, from + ": Error retrieving title: " + err);
+				}else{
 					var message = '';
-					for(var i = 0; i < messages.length; i++) {
-						message += '[' + (i+1) + '] ' + messages[i] + ' ';
+					for(var i = 0; i < results.length; i++) {
+						var result = results[i];
+						if(result != null) {
+							message += '[' + (i+1) + '] ' + result + ' ';
+						}
 					}
-
-					client.say(to, message);
+					message = message.trim();
+					if(message.length > 0) {
+						client.say(to, message);
+					}
 				}
-			}
-
-			urls.forEach(function(url) { urlHandler(url, cb) });
+			});
 		}
 	});
 
@@ -56,7 +54,7 @@ module.exports = function(client, config) {
 		if(parsed.hostname == '127.0.0.1' || parsed.hostname == 'localhost') return cb(null, 'Invalid');
 
 		if(linkCache[url])
-			return;
+			return cb();
 
 		linkCache[url] = {
 			time: Date.now()
@@ -65,9 +63,9 @@ module.exports = function(client, config) {
 		request.head(url, function(err, res, body) {
 			if(err) return cb(err);
 
-			if(res.statusCode != 200) {
-				return cb(null, res.statusCode);
-			}
+			//if(res.statusCode != 200) {
+			//	return cb(null, res.statusCode);
+			//}
 
 			var ct = res.headers['content-type'];
 			if(!ct || ct.match(/^text\/html/i)) {
@@ -102,11 +100,11 @@ module.exports = function(client, config) {
 				var title = $('head title');
 
 				if(title == null) return cb(null, 'No title');
-				title = title.text().trim();
+				title = title.text().replace(/\s+/g, " ").trim();
 				if(title.length == 0) return cb(null, 'No title');
 				return cb(null, title);
 			}catch(err2) {
-				return cb(err2);
+				return cb(null, err2);
 			}
 		});
 	}
